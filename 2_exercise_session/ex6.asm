2480ab0f930b8e7bd95c5766c3f76ad1065df711
Randpy Projects <randpyprojects@gmail.com>
2020-10-14 21:11:23 +0200

    Ex 6 week 2

--- 2_exercise_session/ex6.asm
+++ 2_exercise_session/ex6.asm
@@ -1,20 +1,19 @@
 #TODO
 
 .data
-	array: .string "ABCDEFGH"
+	string: .string "ABCDEFGH"
 	string_length: .space 4
 
 .globl main
 .text
 main:
-	la t0, array
-	li s0, 0
+	la a0, string
+	mv s0, zero
 loop:
-	lb t1, 0(t0)
-	beqz t1, end
+	lb t0, 0(a0)
+	beqz t0, end
 	addi s0, s0, 1
-	addi t1, t1, 1
-	bnez t1, loop
+	addi a0, a0, 1
+	j loop
 end:
-	la a0, string_length
-	sw s0, 0(a0)
+	sb s0, string_length, a1 
