825ae9c9f80cf624c2cabde7f075c475546a7e43
Randpy Projects <randpyprojects@gmail.com>
2020-10-18 11:26:18 +0200

    Ex 5 week 2

--- 2_exercise_session/ex5.asm
+++ 2_exercise_session/ex5.asm
@@ -1,7 +1,7 @@
 .data 
 	array: .word 4, 7, 6, 2, 5, 9 #Better work with size too
 	array_size: .word 6
-	constant: .word 4
+	constant: .word 5
 
 .globl main
 .text
@@ -13,17 +13,23 @@ main:
 	beqz t0, end
 	beq t1, t2, end
 	lw t3, 0(a0)
+	beqz t1, multiply_by_zero
 	mv t4, t1
 multiply_loop:
 	add t5, t5, t3
 	addi t4, t4, -1
-	bne t4, t2, multiply_loop
+	bnez t4, multiply_loop
 	sw t5, 0(a0)
-#array_loop:
 	addi t0, t0, -1
 	addi a0, a0, 4
 	lw t3, 0(a0)
 	mv t4, t1
 	mv t5, zero
-	bne t0, t1, multiply_loop
+	bnez t0, multiply_loop
+	j end
+multiply_by_zero:
+	sw zero, 0(a0)
+	addi a0, a0, 4
+	addi t0, t0, -1
+	bnez t0, multiply_by_zero
 end:
