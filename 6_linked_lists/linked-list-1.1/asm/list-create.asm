f61277a1c273ca177b4227cf24a847f22290d727
JorritWillaert <randpyprojects@gmail.com>
2020-11-16 11:25:43 +0100

    Exercise session 6 function list_create in RISC-V

--- 6_linked_lists/linked-list-1.1/asm/list-create.asm
+++ 6_linked_lists/linked-list-1.1/asm/list-create.asm
@@ -1,5 +1,10 @@
 .text
 .globl list_create
 list_create:
-	#TODO implement
-	ret
+	addi sp, sp, -4
+	sw ra, 0(sp)
+	li a0, 4 #Allocate only 4 bytes
+	jal malloc
+	lw ra, 0(sp)
+	addi sp, sp, 4
+	ret #a0 contains the pointer to the first element, or is 0 if there was not enough space
