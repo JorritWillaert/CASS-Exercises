7a9d1dab814bed9be80ffd20c34a35d411123d45
JorritWillaert <randpyprojects@gmail.com>
2020-11-17 19:44:00 +0100

    Exercise session 6 list_get RISC-V

--- 6_linked_lists/linked-list-1.1/asm/list-get.asm
+++ 6_linked_lists/linked-list-1.1/asm/list-get.asm
@@ -7,5 +7,31 @@
 .text
 .globl list_get
 list_get:
-	#TODO implement
+	#INPUT: a0: list; a1: index; a2: value
+	#OUTPUT: a0: Either OK (-1), UNINITIALIZED_LIST(-1), INDEX_OUT_OF_BOUNDS(-3) or UNINITIALIZED_VALUE (-4)
+	beqz a0, uninitialized_list
+	lw t0, 0(a0) #t0 = node
+	beqz t0, index_out_of_bounds
+	bltz a1, index_out_of_bounds
+	li t1, 0 #t1 = i (counter)
+loop:
+	beq t1, a1, end_loop
+	lw t0, 4(t0)
+	beqz t0, index_out_of_bounds
+	addi t1, t1, 1
+	j loop
+end_loop:
+	beqz a2, uninitialized_retval
+	lw t2, 0(t0) #node->val
+	sw t2, 0(a2)
+	li a0, OK
+	ret
+uninitialized_list:
+	li a0, UNINITIALIZED_LIST
+	ret
+index_out_of_bounds:
+	li a0, INDEX_OUT_OF_BOUNDS
+	ret
+uninitialized_retval:
+	li a0, UNINITIALIZED_RETVAL
 	ret
